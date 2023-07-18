17f5d18d8ac111f2e376713ed9f165858f7541b0
Aditya <adityraj6508@gmail.com>
2023-07-18 23:31:21 +0530

    Renamed the testing file to testing.py

--- game.py
+++
@@ -1,8 +0,0 @@
-from checker import Checker
-
-checker = Checker()
-file_path = "test_cases/Test_L3_R1"
-
-START_TEST = 0
-NUM_TEST = -1
-checker.check(file_path=file_path, start=START_TEST, num_test=NUM_TEST)
